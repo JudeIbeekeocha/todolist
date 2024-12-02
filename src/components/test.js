bdd3a98a29c3ecb1b7f0ddee806d1af73f12c13a
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-12-02 10:32:39 -0500

    Initialized webpack configuration and src directory

---
+++ src/components/test.js
@@ -0,0 +1,4 @@
+export function greetings(){
+    console.log('Hello world from Jude')
+    document.querySelector('h1').textContent = 'Changed with JS'
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { greetings } from "./components/test.js";
+
+greetings()
