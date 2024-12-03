3ced94790c35c8f243ef7a864d0fe3c7de5c235d
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-12-03 07:46:06 -0500

    Form validation

--- src/components/pageviews/renderForms.js
+++ src/components/pageviews/renderForms.js
@@ -3,20 +3,22 @@ import '../styles/formstyles.css'
 export function renderForms(){
     const formsHTML = `
      <div class="container form">
-            <div class="mb-3">
-                <input type="text" class="form-control task-input" id="exampleFormControlInput1" placeholder="Task...">
-              </div>
+            <form id="taskForm">
               <div class="mb-3">
-                <textarea class="form-control text-area" id="exampleFormControlTextarea1" rows="2"></textarea>
-              </div>
-              <div class="date-button-field">
-                  <div class="date">
-                    <input type="date" class="calender">
-                  </div>
-                  <div>
-                    <button class="add-task-button">ADD</button>
-                  </div>
-              </div>
+                  <input type="text" class="form-control task-input" id="exampleFormControlInput1" placeholder="Task..." required>
+                </div>
+                <div class="mb-3">
+                  <textarea class="form-control text-area" id="exampleFormControlTextarea1" rows="2"></textarea>
+                </div>
+                <div class="date-button-field">
+                    <div class="date">
+                      <input type="date" class="calender">
+                    </div>
+                    <div>
+                      <button type="submit" class="add-task-button">ADD</button>
+                    </div>
+                </div>
+            </form>
         </div>
     `
     document.querySelector('.form-container').innerHTML = formsHTML
